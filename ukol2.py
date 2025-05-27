b04e387512aaaacc4fd483d7c3cc03a461365aa4
unknown <alina.leshchenko@infis.cz>
2025-05-27 09:14:40 +0200

    32

--- ukol2.py
+++ ukol2.py
@@ -1,21 +1,32 @@
 
-import random
+from funkce import pozdrav, dnesni_den, vypis_cisla, obdelnik, nalada_dne
 
-def pozdrav():
-    print("Ahoj, jak se máš?")
+def main():
+    while True:
+        print("--- MENU ---")
+        print("1. Pozdrav")
+        print("2. Dnešní den")
+        print("3. Čísla 1 až 5")
+        print("4. Obdélník z hvězdiček")
+        print("5. Nálada dne")
+        print("6. Konec")
+        volba = input("Zadej číslo: ")
 
-def dnesni_den():
-    print("Dnes je úterý.")
+        if volba == "1":
+            pozdrav()
+        elif volba == "2":
+            dnesni_den()
+        elif volba == "3":
+            vypis_cisla()
+        elif volba == "4":
+            obdelnik()
+        elif volba == "5":
+            nalada_dne()
+        elif volba == "6":
+            print("Program končí.")
+            break
+        else:
+            print("Neplatná volba, zkus to znovu.")
 
-def vypis_cisla():
-    for i in range(1, 6):
-        print(i, end=' ')
-    print()
-
-def obdelnik():
-    for _ in range(3):
-        print("*****")
-
-def nalada_dne():
-    nalady = ["veselá", "smutná", "unavená", "nadšená", "zamračená"]
-    print(f"Dnešní nálada je: {random.choice(nalady)}.")
+if __name__ == "__main__":
+    main()
